62627c50c4af4920dcf0b6f5310877b50bb9fa97
Daniel Santos <daniel.santos@datastax.com>
2022-12-24 09:44:43 -0400

    go code connects to the database just fine now

--- packages/api/addImageToDog.go
+++ packages/api/addImageToDog.go
@@ -3,39 +3,88 @@ package main
 // https://docs.aws.amazon.com/lambda/latest/dg/golang-handler.html
 // https://github.com/aws/aws-lambda-go/
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"io"
+	"net/http"
 )
 
-//type ResponseBody struct {
-//	Foo string `json:"foo"`
-//	Bar string `json:"bar"`
-//}
+type RequestBody struct {
+	Category string `json:"category"`
+	Entry    string `json:"entry"`
+}
 
 func HandleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body size = %d.\n", len(request.Body))
-	fmt.Println("Headers:")
-	for key, value := range request.Headers {
-		fmt.Printf("    %s: %s\n", key, value)
-	}
-	stringifiedRequest, _ := json.Marshal(request)
-	//	prepare response
-	//body := ResponseBody{
-	//	Foo: string(stringifiedRequest),
-	//Bar: string(stringifiedRequest),
+	//fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	//fmt.Printf("Body size = %d.\n", len(request.Body))
+	//fmt.Println("Headers:")
+	//for key, value := range request.Headers {
+	//	fmt.Printf("    %s: %s\n", key, value)
 	//}
 
-	//bodyStringified, _ := json.Marshal(body)
+	// return early if path is not "path": "/games",
+	if request.Path != "/games" {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 404,
+			Body:       "Not Found",
+		}, nil
+	}
+
+	// evaluate if required parameters are present
+	if request.QueryStringParameters["category"] == "" || request.QueryStringParameters["entry"] == "" {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 400,
+			Body:       "Bad Request",
+		}, nil
+	}
+	// queryStringParameters
+	requestParams := RequestBody{
+		Category: request.QueryStringParameters["category"],
+		Entry:    request.QueryStringParameters["entry"],
+	}
+
+	goodBody, err := json.Marshal(requestParams)
+
+	host := "https://" + request.StageVariables["DB"] + "-" + request.StageVariables["REGION"] + ".apps.astra.datastax.com"
+	path := "/api/rest/v2/keyspaces/" + request.StageVariables["KEYSPACE_2"] + "/" + request.StageVariables["TABLE_2"]
+
+	// call database with the existing parameters
+	customRequest, err := http.NewRequest(http.MethodPost, host+path, bytes.NewReader(goodBody))
+	if err != nil {
+		// handle error
+		fmt.Print(err.Error())
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       "Something went wrong with the request creation",
+		}, nil
+	}
+	customRequest.Header.Set("Content-Type", "application/json")
+	customRequest.Header.Set("x-cassandra-token", request.StageVariables["TOKEN"])
+
+	client := &http.Client{}
+	resp, err := client.Do(customRequest)
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}(customRequest.Body)
+	// have the body converted as bytes[]
+	parsedBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 	response := events.APIGatewayProxyResponse{
-		StatusCode:        201,
-		Headers:           nil,
+		StatusCode:        resp.StatusCode,
+		Headers:           map[string]string{},
 		MultiValueHeaders: nil,
-		Body:              string(stringifiedRequest),
+		Body:              string(parsedBody),
 		IsBase64Encoded:   false,
 	}
 
