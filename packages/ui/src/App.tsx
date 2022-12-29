0bb8471ccadd1c7fe3983ab98810a2cc655665c0
Daniel Santos <daniel.santos@datastax.com>
2022-12-29 15:10:19 -0400

    updates in the logic to render dogs

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -5,78 +5,95 @@ const myHeaders = new Headers();
 myHeaders.append("x-api-key", process.env?.REACT_APP_AWS_API_KEY ?? "foo");
 // don't forget to add on the server: "Access-Control-Allow-Origin = *"
 
-const requestOptions = {
-	method: "GET",
-	headers: myHeaders,
-};
-
-interface Membro {
+interface ApiResponseType {
 	relatedimages: string[],
 	baseimages: string[],
 	name: string,
 	cat: string,
 }
 
-const CaoHTML = ({membro, isBaseImage}: { membro: Membro, isBaseImage: boolean }) => {
+const SingleEntryElement = ({membro, isBaseImage, onClick}: { membro: ApiResponseType, isBaseImage: boolean, onClick?: () => void }) => {
 	return (
-		<section className={"membro"} key={membro.name}>
+		<section className={"membro"} onClick={onClick}>
 			<img
-				src={process.env?.REACT_APP_CDN_URL + "/" + isBaseImage ? membro.baseimages[0] : membro.relatedimages[0]}
+				src={isBaseImage ? process.env?.REACT_APP_CDN_URL + "/" + membro.baseimages[0] : process.env?.REACT_APP_CDN_URL + "/" + membro.relatedimages[0]}
 				alt={membro.name}/>
 			<pre style={{color: "black", textAlign: "center"}}>{membro.name}</pre>
 		</section>
 	)
 };
 
+const handleAnswer = (isCorrect: boolean) => {
+	alert(isCorrect ? "✅" : "❌");
+}
+
+const handleUserRegistration = (email: string, setUserRetrieved: (v: boolean) => void) => {
+	if (email === "") {
+		return
+	}
+	// make fetch call to register user
+	setUserRetrieved(true);
+}
+
 function App() {
-	const [membros, setMembros] = useState({
-		count: 0,
-		data: [
-			{
-				relatedimages: [],
-				baseimages: [],
-				name: "",
-				cat: "",
-			},
-		],
-	});
+	const [userEmail, setUserEmail] = useState<string>("");
+	const [userRetrieved, setUserRetrieved] = useState<boolean>(false);
+	const [apiResponse, setApiResponse] = useState<ApiResponseType[]>([]);
 	useEffect(() => {
 		const fetchPatrulhaCanina = async () => {
 			const response = await fetch(
 				process.env?.REACT_APP_AWS_API_URL + "/v3" ?? "foo",
-				requestOptions
+				{
+					method: "GET",
+					headers: myHeaders,
+				}
 			);
 			const data = await response.json();
-			// console.log(data);
-			setMembros(data);
+			setApiResponse(data.data);
 			return data;
 		};
 		fetchPatrulhaCanina().then(_ => null);
 	}, []);
 
-	const options = membros.data.map((e, i) => {
-		return i
-	});
-	const randomOption = () => {
-		const randomPointerInRange = Math.trunc(Math.random() * options.length);
-		const valueLeftFound = options[randomPointerInRange !== options.length ? randomPointerInRange : options.length - 1];
-		options.splice(randomPointerInRange, 1)
-		return valueLeftFound
-	}
 	return (
 		<div className="App">
+			<header style={{display: "flex", flexDirection: "column"}}>
+				<p style={{textAlign: "center"}}>Patrulha Canina</p>
+				{!userRetrieved &&
+					(<><label htmlFor="email" style={{flex: 1, textAlign: "center"}}>INSIRA SEU USUARIO:</label>
+						<section style={{display: "flex", padding: "1rem", maxWidth: "400px", margin: "auto"}}>
+							<input type="email" style={{flex: 1}} inputMode={"email"} translate={"no"} autoFocus={true}
+								   id={'email'}
+								   value={userEmail}
+								   onChange={v => setUserEmail(v.currentTarget.value)}
+								   onKeyDown={(ev) => ev.key==='Enter' ? handleUserRegistration(userEmail, setUserRetrieved): null}
+							/>
+							<button
+								onClick={() => handleUserRegistration(userEmail, setUserRetrieved)}
+								disabled={userRetrieved}>OK
+							</button>
+						</section>
+					</>)}
+			</header>
 			<section className={"cima"}>
-				<CaoHTML isBaseImage={true} membro={membros.data[randomOption()]}/>
+				{apiResponse.length > 0 && <SingleEntryElement isBaseImage={true} membro={apiResponse[0]}/>}
 				<section>
-					<h2>✅: 0</h2>
-					<h2>❌: 0</h2>
+					{userRetrieved && <div style={{flex: 1}} id={'email'}>{userEmail}</div>}
+					<h2>✅: {userRetrieved ? 0 : "n/a"}</h2>
+					<h2>❌: {userRetrieved ? 0 : "n/a"}</h2>
 				</section>
 			</section>
 
-			<section className={"baixo"}>
-				<CaoHTML isBaseImage={false} membro={membros.data[randomOption()]}/>
-				<CaoHTML isBaseImage={false} membro={membros.data[randomOption()]}/>
-			</section>
+			{userRetrieved &&
+				<section className={"baixo"}>
+					{apiResponse.length > 0 &&
+						<SingleEntryElement isBaseImage={false} membro={apiResponse[0]} onClick={() => handleAnswer(true)}/>}
+					{apiResponse.length > 0 &&
+						<SingleEntryElement isBaseImage={false} membro={apiResponse[1]} onClick={() => handleAnswer(false)}/>}
+					{apiResponse.length > 0 &&
+						<SingleEntryElement isBaseImage={false} membro={apiResponse[2]} onClick={() => handleAnswer(false)}/>}
+				</section>
+			}
 		</div>
 	);
 }
