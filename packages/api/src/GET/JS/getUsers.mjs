ad2075c75b5844277c96c3846dddb232a79758be
Daniel Santos <daniel.santos@datastax.com>
2023-01-01 23:50:05 -0400

    added validation for get. works perfectly

--- packages/api/src/GET/JS/getUsers.mjs
+++ packages/api/src/GET/JS/getUsers.mjs
@@ -1,25 +1,34 @@
 export const handler = async (event) => {
-	console.log({event});
-	const {httpMethod, stageVariables, path, requestContext} = event;
-	const {DB, REGION, TABLE_3_USERS, KEYSPACE, TOKEN} = stageVariables;
+	// console.log({event});
+	const {httpMethod, stageVariables, path, requestContext, queryStringParameters} = event;
+	const {DB, REGION, TABLE_USERS, KEYSPACE, TOKEN} = stageVariables;
+
+	// Return if the required params are not present
+	if (!Object.keys(queryStringParameters).includes('cat') || !Object.keys(queryStringParameters).includes('email')) {
+		return {
+			statusCode: 400, body: JSON.stringify({
+				message: 'Missing required parameter',
+			}),
+		};
+	}
+
 	const host = `https://${DB}-${REGION}.apps.astra.datastax.com`
 
 	if (httpMethod !== 'GET') {
 		return {
-			statusCode: 405,
-			body: JSON.stringify({error: 'Method not allowed'})
+			statusCode: 405, body: JSON.stringify({error: 'Method not allowed'})
 		};
 	}
 
 	if (path.split('/')[1] !== 'users') {
 		return {
-			statusCode: 404,
-			body: JSON.stringify({error: 'Not Found'}),
+			statusCode: 404, body: JSON.stringify({error: 'Not Found'}),
 		};
 	}
 
 	// TODO: add params to query for specific user
-	const evaluatedPath = `/api/rest/v2/keyspaces/${KEYSPACE}/${TABLE_3_USERS}`;
+	const evaluatedPath = `/api/rest/v2/keyspaces/${KEYSPACE}/${TABLE_USERS}`;
+	const where = `?where={"cat":{"$eq":"${queryStringParameters.cat}"},"email":{"$eq":"${queryStringParameters.email}"}}`;
 
 	const headers = {
 		'content-type': 'application/json',
@@ -27,13 +36,11 @@ export const handler = async (event) => {
 		'Access-Control-Allow-Origin': requestContext?.domainName ?? "*", //TODO: add func to filter origins
 	}
 
-	const respPromise = await fetch(host + evaluatedPath, {
-		method: 'GET',
-		headers: headers
+	const respPromise = await fetch(host + evaluatedPath + where, {
+		method: 'GET', headers: headers
 	})
 	const respParsed = await respPromise.json();
 	return {
-		statusCode: 201,
-		body: JSON.stringify(respParsed),
+		statusCode: 201, body: JSON.stringify(respParsed),
 	};
 };
