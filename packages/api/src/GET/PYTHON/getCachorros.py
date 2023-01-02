a2e161abcc71035e54c859e47eb469e57d453844
Daniel Santos <daniel.santos@datastax.com>
2023-01-01 20:08:51 -0400

    python code leverages env variables from AWS API Gateway

--- packages/api/src/GET/PYTHON/getCachorros.py
+++ packages/api/src/GET/PYTHON/getCachorros.py
@@ -5,24 +5,26 @@ import urllib.parse
 
 def lambda_handler(event, context):
 	# print(event)
-	conn = http.client.HTTPSConnection("76644382-5d3f-4b73-81f9-c4739122ba0d-us-east-1.apps.astra.datastax.com")
+	conn = http.client.HTTPSConnection(
+		event['stageVariables']['DB'] + '-' + event['stageVariables']['REGION'] + '.apps.astra.datastax.com')
 	payload = ''
+	params = ''
 	headers = {
 		'content-type': 'application/json',
-		'x-cassandra-token': 'AstraCS:GUvJLheoXoeDTUJnxPJLjYek:24fcee4fd9f4aea550750fa9ad9eaa8594633e0f0870e955c17aec66b361cd44'
+		'x-cassandra-token': event['stageVariables']['TOKEN']
 	}
 	domain = "foo/bar"
 	request_path = event['path']
 	request_path_array = request_path.split('/')
 	relevant_path = request_path_array[1]
-	# print(relevant_path)
 	# Check for WHICH PATH DID THE REQUEST CAME THROUGH
-	# if dogs, use ENV keyspace_1/table_1
-	# if games, use ENV keyspace_2/table_2
-	if relevant_path == "dogs":
-		domain = event['stageVariables']['KEYSPACE_1'] + '/' + event['stageVariables']['TABLE_1']
-	if relevant_path == "games":
-		domain = event['stageVariables']['KEYSPACE_2'] + '/' + event['stageVariables']['TABLE_2']
+	# keyspace/table are not guaranteed to exist
+	if relevant_path == "categories":
+		domain = event['stageVariables']['KEYSPACE'] + '/' + event['stageVariables']['TABLE_CATEGORIES']
+	if relevant_path == "entries":
+		domain = event['stageVariables']['KEYSPACE'] + '/' + event['stageVariables']['TABLE_ENTRIES']
+	if relevant_path == "users":
+		domain = event['stageVariables']['KEYSPACE'] + '/' + event['stageVariables']['TABLE_USERS']
 
 	url_final = "/api/rest/v2/keyspaces/" + domain + "/rows"
 	# }'
@@ -41,6 +43,22 @@ def lambda_handler(event, context):
 					"$eq": event['queryStringParameters']['category']
 				}
 			}
+		if 'cat' in dict(event['queryStringParameters']).keys():
+			if 'name' in dict(event['queryStringParameters']).keys():
+				params = {
+					"cat": {
+						"$eq": event['queryStringParameters']['cat']
+					},
+					"name": {
+						"$eq": event['queryStringParameters']['name']
+					}
+				}
+			else:
+				params = {
+					"cat": {
+						"$eq": event['queryStringParameters']['cat']
+					}
+				}
 		# this is the build inside the if
 		url_final = "/api/rest/v2/keyspaces/" + domain + "?where=" + urllib.parse.quote(
 			json.dumps(params))
