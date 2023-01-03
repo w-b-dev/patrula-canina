d6cf7a12286e306fa3c54526077c400c3feb65ac
Daniel Santos <daniel.santos@datastax.com>
2023-01-02 22:58:03 -0400

    fix CORS and add checks for APIs

--- packages/api/src/GET/JS/getUsers.mjs
+++ packages/api/src/GET/JS/getUsers.mjs
@@ -1,28 +1,45 @@
 export const handler = async (event) => {
 	// console.log({event});
-	const {httpMethod, stageVariables, path, requestContext, queryStringParameters} = event;
-	const {DB, REGION, TABLE_USERS, KEYSPACE, TOKEN} = stageVariables;
+	const {
+		httpMethod,
+		stageVariables,
+		path,
+		queryStringParameters,
+	} = event;
+	if (!stageVariables || Object.keys(stageVariables).length === 0) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: "No stage variables provided." }),
+		};
+	}
+	const { DB, REGION, TABLE_USERS, KEYSPACE, TOKEN } = stageVariables;
 
 	// Return if the required params are not present
-	if (!Object.keys(queryStringParameters).includes('cat') || !Object.keys(queryStringParameters).includes('email')) {
+	if (
+		!Object.keys(queryStringParameters).includes("cat") ||
+		!Object.keys(queryStringParameters).includes("email")
+	) {
 		return {
-			statusCode: 400, body: JSON.stringify({
-				message: 'Missing required parameter',
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "Missing required parameter",
 			}),
 		};
 	}
 
-	const host = `https://${DB}-${REGION}.apps.astra.datastax.com`
+	const host = `https://${DB}-${REGION}.apps.astra.datastax.com`;
 
-	if (httpMethod !== 'GET') {
+	if (httpMethod !== "GET") {
 		return {
-			statusCode: 405, body: JSON.stringify({error: 'Method not allowed'})
+			statusCode: 405,
+			body: JSON.stringify({ error: "Method not allowed" }),
 		};
 	}
 
-	if (path.split('/')[1] !== 'users') {
+	if (path.split("/")[1] !== "users") {
 		return {
-			statusCode: 404, body: JSON.stringify({error: 'Not Found'}),
+			statusCode: 404,
+			body: JSON.stringify({ error: "Not Found" }),
 		};
 	}
 
@@ -30,16 +47,20 @@ export const handler = async (event) => {
 	const where = `?where={"cat":{"$eq":"${queryStringParameters.cat}"},"email":{"$eq":"${queryStringParameters.email}"}}`;
 
 	const headers = {
-		'content-type': 'application/json',
-		'x-cassandra-token': TOKEN,
-		'Access-Control-Allow-Origin': requestContext?.domainName ?? "*", //TODO: add func to filter origins
-	}
+		"content-type": "application/json",
+		"x-cassandra-token": TOKEN,
+	};
 
 	const respPromise = await fetch(host + evaluatedPath + where, {
-		method: httpMethod, headers: headers
-	})
+		method: httpMethod,
+		headers: headers,
+	});
 	const respParsed = await respPromise.json();
 	return {
-		statusCode: 201, body: JSON.stringify(respParsed),
+		statusCode: 201,
+		body: JSON.stringify(respParsed),
+		headers: {
+			"Access-Control-Allow-Origin": "*", //TODO: add func to filter origins
+		},
 	};
 };
--- packages/api/src/POST/JS/postUsers.mjs
+++ packages/api/src/POST/JS/postUsers.mjs
@@ -1,44 +1,67 @@
 export const handler = async (event) => {
 	// console.log({event});
-	const {httpMethod, stageVariables, path, requestContext, queryStringParameters} = event;
-	const {DB, REGION, TABLE_USERS, KEYSPACE, TOKEN} = stageVariables;
+	const {
+		httpMethod,
+		stageVariables,
+		path,
+		queryStringParameters,
+	} = event;
+	if (!stageVariables || Object.keys(stageVariables).length === 0) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: "No stage variables provided." }),
+		};
+	}
+	const { DB, REGION, TABLE_USERS, KEYSPACE, TOKEN } = stageVariables;
 
 	// Return if the required params are not present
-	if (!Object.keys(queryStringParameters).includes('cat') || !Object.keys(queryStringParameters).includes('email') || !Object.keys(queryStringParameters).includes('name')) {
+	if (
+		!Object.keys(queryStringParameters).includes("cat") ||
+		!Object.keys(queryStringParameters).includes("email") ||
+		!Object.keys(queryStringParameters).includes("name")
+	) {
 		return {
-			statusCode: 400, body: JSON.stringify({
-				message: 'Missing required parameter',
+			statusCode: 400,
+			body: JSON.stringify({
+				message: "Missing required parameter",
 			}),
 		};
 	}
 
-	const host = `https://${DB}-${REGION}.apps.astra.datastax.com`
+	const host = `https://${DB}-${REGION}.apps.astra.datastax.com`;
 
-	if (httpMethod !== 'POST') {
+	if (httpMethod !== "POST") {
 		return {
-			statusCode: 405, body: JSON.stringify({error: 'Method not allowed'})
+			statusCode: 405,
+			body: JSON.stringify({ error: "Method not allowed" }),
 		};
 	}
 
-	if (path.split('/')[1] !== 'users') {
+	if (path.split("/")[1] !== "users") {
 		return {
-			statusCode: 404, body: JSON.stringify({error: 'Not Found'}),
+			statusCode: 404,
+			body: JSON.stringify({ error: "Not Found" }),
 		};
 	}
 
 	const evaluatedPath = `/api/rest/v2/keyspaces/${KEYSPACE}/${TABLE_USERS}`;
 
 	const headers = {
-		'content-type': 'application/json',
-		'x-cassandra-token': TOKEN,
-		'Access-Control-Allow-Origin': requestContext?.domainName ?? "*", //TODO: add func to filter origins
-	}
+		"content-type": "application/json",
+		"x-cassandra-token": TOKEN,
+	};
 
 	const respPromise = await fetch(host + evaluatedPath, {
-		method: httpMethod, headers: headers, body: JSON.stringify(queryStringParameters),
-	})
+		method: httpMethod,
+		headers: headers,
+		body: JSON.stringify(queryStringParameters),
+	});
 	const respParsed = await respPromise.json();
 	return {
-		statusCode: 201, body: JSON.stringify(respParsed),
+		statusCode: 201,
+		body: JSON.stringify(respParsed),
+		headers: {
+			"Access-Control-Allow-Origin": "*", //TODO: add func to filter origins
+		},
 	};
 };
