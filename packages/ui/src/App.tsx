e001ce2fc262e0739cc382e65fd132826c7ac9e9
Daniel Santos <daniel.santos@datastax.com>
2023-01-02 23:01:31 -0400

    removal of handlers, added postUsers and getUsers

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,76 +1,121 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import {ApiResponseType} from "./interfaces";
-import {SingleEntryElement} from "./SingleEntryElement";
-import {getAllEntries, getUser} from "./api";
-
-
-const handleAnswer = (isCorrect: boolean) => {
-	alert(isCorrect ? "✅" : "❌");
-}
-
-const handleUserRegistration = (email: string, setUserRetrieved: (v: boolean) => void) => {
-	if (email === "") {
-		return
-	}
-	// make fetch call to register user
-	getUser().then((data) => {
-		alert(JSON.stringify(data.data[0]));
-	});
-	setUserRetrieved(true);
-}
+import { ApiResponseType, User } from "./interfaces";
+import { SingleEntryElement } from "./SingleEntryElement";
+import { getAllEntries } from "./api";
+import { handleUserRegistration, handleAnswer } from "./Handlers";
 
 function App() {
 	const [userEmail, setUserEmail] = useState<string>("");
-	const [userRetrieved, setUserRetrieved] = useState<boolean>(false);
+	const [userRetrieved, setUserRetrieved] = useState<User | null>(null);
 	const [getAPI, setGetAPI] = useState<ApiResponseType[]>([]);
 	useEffect(() => {
-		getAllEntries().then(data => setGetAPI(data.data)
-		);
+		getAllEntries().then((data) => setGetAPI(data.data));
 	}, []);
 
 	return (
 		<div className="App">
-			<header style={{display: "flex", flexDirection: "column"}}>
-				<p style={{textAlign: "center"}}>Patrulha Canina</p>
-				{!userRetrieved &&
-					(<><label htmlFor="email" style={{flex: 1, textAlign: "center"}}>INSIRA SEU USUARIO:</label>
-						<section style={{display: "flex", padding: "1rem", maxWidth: "400px", margin: "auto"}}>
-							<input type="email" style={{flex: 1}} inputMode={"email"} translate={"no"} autoFocus={true}
-								   id={'email'}
-								   value={userEmail}
-								   onChange={v => setUserEmail(v.currentTarget.value)}
-								   onKeyDown={(ev) => ev.key === 'Enter' ? handleUserRegistration(userEmail, setUserRetrieved) : null}
+			<header style={{ display: "flex", flexDirection: "column" }}>
+				<h1
+					style={{
+						textAlign: "center",
+						margin: 0,
+						padding: 0,
+						textDecoration: "underline",
+						color: "#222",
+					}}
+				>
+					Patrulha Canina
+				</h1>
+				{!userRetrieved && (
+					<>
+						<label
+							htmlFor="email"
+							style={{ flex: 1, textAlign: "center", marginTop: "1rem" }}
+						>
+							USERNAME:
+						</label>
+						<section
+							style={{
+								display: "flex",
+								padding: "0 1rem",
+								maxWidth: "400px",
+								margin: "auto",
+							}}
+						>
+							<input
+								type="email"
+								style={{ flex: 1 }}
+								inputMode={"email"}
+								translate={"no"}
+								autoFocus={true}
+								id={"email"}
+								value={userEmail}
+								onChange={(v) => setUserEmail(v.currentTarget.value)}
+								onKeyDown={(ev) =>
+									ev.key === "Enter"
+										? handleUserRegistration(userEmail, setUserRetrieved)
+										: null
+								}
 							/>
 							<button
-								onClick={() => handleUserRegistration(userEmail, setUserRetrieved)}
-								disabled={userRetrieved}>OK
+								onClick={() =>
+									handleUserRegistration(userEmail, setUserRetrieved)
+								}
+								disabled={!!userRetrieved}
+							>
+								OK
 							</button>
 						</section>
-					</>)}
+					</>
+				)}
 			</header>
 			<section className={"cima"}>
-				{getAPI.length > 0 && <SingleEntryElement isBaseImage={true} membro={getAPI[0]}/>}
+				{getAPI.length > 0 && (
+					<SingleEntryElement isBaseImage={true} membro={getAPI[0]} />
+				)}
 				<section>
-					{userRetrieved && <div style={{flex: 1}} id={'email'}>{userEmail}</div>}
-					<h2>✅: {userRetrieved ? 0 : "n/a"}</h2>
-					<h2>❌: {userRetrieved ? 0 : "n/a"}</h2>
+					{userRetrieved && (
+						<h2 style={{ flex: 1 }} id={"email"}>
+							{userRetrieved.name}
+						</h2>
+					)}
+					<h3>✅: {userRetrieved ? userRetrieved.won : "n/a"}</h3>
+					<h3>❌: {userRetrieved ? userRetrieved.lost : "n/a"}</h3>
+					<button
+						onClick={() => window.location.reload()}
+						disabled={!userRetrieved}
+					>
+						Sair
+					</button>
 				</section>
 			</section>
 
-			{userRetrieved &&
+			{userRetrieved && (
 				<section className={"baixo"}>
-					{getAPI.length > 0 &&
-						<SingleEntryElement isBaseImage={false} membro={getAPI[0]}
-											onClick={() => handleAnswer(true)}/>}
-					{getAPI.length > 0 &&
-						<SingleEntryElement isBaseImage={false} membro={getAPI[1]}
-											onClick={() => handleAnswer(false)}/>}
-					{getAPI.length > 0 &&
-						<SingleEntryElement isBaseImage={false} membro={getAPI[2]}
-											onClick={() => handleAnswer(false)}/>}
+					{getAPI.length > 0 && (
+						<SingleEntryElement
+							isBaseImage={false}
+							membro={getAPI[0]}
+							onClick={() => handleAnswer(true)}
+						/>
+					)}
+					{getAPI.length > 0 && (
+						<SingleEntryElement
+							isBaseImage={false}
+							membro={getAPI[1]}
+							onClick={() => handleAnswer(false)}
+						/>
+					)}
+					{getAPI.length > 0 && (
+						<SingleEntryElement
+							isBaseImage={false}
+							membro={getAPI[2]}
+							onClick={() => handleAnswer(false)}
+						/>
+					)}
 				</section>
-			}
+			)}
 		</div>
 	);
 }
--- packages/ui/src/SingleEntryElement.tsx
+++ packages/ui/src/SingleEntryElement.tsx
@@ -1,17 +1,36 @@
-import {ApiResponseType} from "./interfaces";
+import { ApiResponseType } from "./interfaces";
 import React from "react";
 
 export const SingleEntryElement = ({
-									   membro,
-									   isBaseImage,
-									   onClick
-								   }: { membro: ApiResponseType, isBaseImage: boolean, onClick?: () => void }) => {
+	membro,
+	isBaseImage,
+	onClick,
+}: {
+	membro: ApiResponseType;
+	isBaseImage: boolean;
+	onClick?: () => void;
+}) => {
 	return (
 		<section className={"membro"} onClick={onClick}>
 			<img
-				src={isBaseImage ? process.env?.REACT_APP_CDN_URL + "/" + membro.baseimages[0] : process.env?.REACT_APP_CDN_URL + "/" + membro.relatedimages[0]}
-				alt={membro.name}/>
-			<pre style={{color: "black", textAlign: "center"}}>{membro.name}</pre>
+				src={
+					isBaseImage
+						? process.env?.REACT_APP_CDN_URL + "/" + membro.baseimages[0]
+						: process.env?.REACT_APP_CDN_URL + "/" + membro.relatedimages[0]
+				}
+				alt={membro.name}
+			/>
+			<h4
+				style={{
+					color: "#222",
+					fontWeight: "lighter",
+					textAlign: "center",
+					margin: 0,
+					padding: 0,
+				}}
+			>
+				{membro.name}
+			</h4>
 		</section>
-	)
+	);
 };
